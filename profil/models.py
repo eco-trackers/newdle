22abce94f182d34cb1361afc944c5a6d70582d65
quentinproject <girardatquentin@gmail.com>
2024-06-04 10:54:15 +0200

    fixed profil class with group

--- profil/models.py
+++ profil/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from group.models import Group
 
 class Profil(models.Model):
     User = get_user_model()
-    user = models.ForeignObject(User,on_delete=models.CASCADE)
-    Type = models.CharField(max_length=1)
-    group = models.ForeignObject(Group,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    type = models.CharField(max_length=1)
+    group = models.ManyToManyField(Group,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user
