ce71f17c7fe1b6417890e04c04f161016d2d778f
mehdirussel <35711089+mehdirussel@users.noreply.github.com>
2024-06-04 11:19:24 +0200

    modele matiere

--- subjects/models.py
+++ subjects/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-
-# Create your models here.
-class Subject(models.model):
-    pass
+from profil.models import Profil
 
 class UE(models.model):
     pass
+
+class Subject(models.model):
+    name = models.CharField(max_length=50)
+    ue = models.ManyToManyField(to=UE,on_delete=models.CASCADE)
+    prof = models.ManyToManyField(to=Profil,on_delete=models.CASCADE)
+    student_group = models.ManyToManyField(to=Group,on_delete=models.CASCADE)
+    coef = models.CharField(max_length=3)
+
+
+    def __str__(self):
+        return self.name + '|' + str(self.ue) + '|'  + str(self.prof) +'|' + str(self.student_group)
