a64f6399add52672d349051f7015830a902a1155
Victor Murris <murrisvictor@gmail.com>
2024-06-04 10:35:09 +0200

    absence model

---
+++ absence/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+class Absence :
+    status = models.CharField(max_length=1)
+    student = models.ForeignKey(Profil, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    
+    def __str__(self):
+        return self.status
