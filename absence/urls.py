bbd40ab7af068dd0a571a45ffd8b04e55103c8f0
Victor Murris <murrisvictor@gmail.com>
2024-06-05 14:17:51 +0200

    Absence edit-supp

--- absence/urls.py
+++ absence/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('../', views.index_view, name='index'),
     path('<str:id>/', views.AbsenceView.as_view(), name='absence'),
     path('', views.AbsenceView.as_view(), name='absence_none'),
+    path('<str:id>/delete/', views.delete_absence, name='absence_delete'),
+    path('<int:id>/edit/', views.edit_absence, name='absence_edit'),
 ]
