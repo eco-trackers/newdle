506983f9e9f0d01db924352fefdd8b4764d07afa
Victor Murris <murrisvictor@gmail.com>
2024-06-05 09:26:21 +0200

    bug resolve

--- newdle/urls.py
+++ newdle/urls.py
@@ -20,5 +20,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', include('login.urls')),
-    path('absence/', include('absence.urls')),
 ]
