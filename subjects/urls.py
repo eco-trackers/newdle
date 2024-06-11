1554c398491897c45f579c9ca701a9e4680eb72c
TheGeekMax <maximesanciaume@gmail.com>
2024-06-11 17:07:01 +0200

    tt

--- subjects/urls.py
+++ subjects/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 
+
 app_name='subjects'
+
 urlpatterns = [
     path('',views.subjects_home_view,name="subjects-home-view"),
     path('<int:subject_id>/',views.subjects_get_detail_view,name='subjects-get-detail-view'),
