84a9f231bc726e24cc68899cfa8f31ab59d22fac
TheGeekMax <maximesanciaume@gmail.com>
2024-06-12 09:27:14 +0200

    added list notes + begining of all things

--- notes/urls.py
+++ notes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = 'notes'
-
 urlpatterns = [
-    path('',views.list_notes_view,name="liste.view"),
-    path('create/',views.create_notes_view,name="create.view")   
+    path('',views.list_view,name="list.view"),
+    path('edit/',views.edit_view,name="edit.view"),
+    path('edit/<int:id>',views.edit_detail,name="detail.view"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,35 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
 from django.contrib.auth.decorators import login_required
 from .forms import NoteCreateForm
-from subjects.models import Subject
+from subjects.models import Subject,UE
 from profil.models import Profil
 
 
 # def get_prof_subjects(request):
 #     return Subject.objects.filter(prof=request.user.profil).all()
 
-def get_UEs(request,semester):
-    # step 0 - get all subjects for user that is in the group
-    subjects = Subject.objects.filter(student_group=request.user.profil.group).all()
+def get_all_student_notes(request):
+    """
+        format
+        [
+            {
+                "subject":"subjectName",
+                "UE":UE,
+                "coef":coefValue,
+                "note":noteValue (default -1)
+            },
+            ...
+        ]
+    """
+    notes = []
+    subjects = Subject.objects.all()
+    for subject in subjects:
+        # get not if exists
+        note = -1
+        try:
+            note = int("{:,.0f}".format(Note.objects.get(profil=request.user.profil,subject=subject).valeur))
+        except:
+            pass
 
-    # step 1 - get all UE from subjects list
-    
-
-def get_student_notes(request,subject):
-    return Note.objects.filter(profil=request.user.profil,subject=subject).all()
+        notes.append({
+            "subject":subject.name,
+            "UE":subject.ue,
+            "coef":subject.coef,
+            "note":note
+        })
+    return notes
 
 def get_formatted_notes(request):
     """
         in format
         [
             {
-                "name":"UEName",
+                "name":"UE",
                 "count":noteCount,
                 "notes":[
                     {
                         "subject":"subjectName",
-                        "note":noteValue (default -1)
+                        "note":noteValue (default -1),
+                        "coef":coefValue
                     },
                     ...
                 ]
@@ -37,30 +59,80 @@ def get_formatted_notes(request):
             ...
         ]
     """
-    subjects = get_student_subjects(request)
-    notes = []
+    subjects = get_all_student_notes(request)
+    ues = []
+    ues_list = []
     for subject in subjects:
+        ue = subject["UE"]
+        if ue not in ues_list:
+            ues_list.append(ue)
+            ues.append({
+                "name":ue,
+                "count":0,
+                "notes":[]
+            })
         
+        for ue in ues:
+            if ue["name"] == subject["UE"]:
+                ue["count"] += 1
+                ue["notes"].append({
+                    "subject":subject["subject"],
+                    "note":subject["note"],
+                    "coef":subject["coef"]
+                })
+    print(ues)
+    return ues
+        
+def get_notes(request,subject_id):
+    """
+        format
+        [
+            {
+                "Studdent":Profil,
+                "note":noteValue (default -1)
+            },
+            ...
+        ]
+    """
+
+    notes = []
+    # get subject group
+    subject = Subject.objects.get(id=subject_id)
+    # get all students in the group
+    students = Profil.objects.filte
 
 
-def list_notes_view(request):
+# views
+
+@login_required
+def list_view(request):
+    # test which role the user has
+    if int(Profil.objects.get(user=request.user).type) > 0:
+        return redirect('notes:edit.view')
+
+    # if not redirect => you are a studdent
+
     # Retrieve all notes from the database
-    notes = Note.objects.all()
+    ues = get_formatted_notes(request)
 
     # Render the template with the list of notes
-    return render(request, 'list_notes.html', {'notes': notes})
+    return render(request, 'notes/list_notes.html', {'ues': ues})
 
 
+@login_required
+def edit_view(request):
+    # test which role the user has
+    subjects = []
+    match int(Profil.objects.get(user=request.user).type):
+        case 0:
+            redirect('notes:list.view')
+        case 1:
+            subjects = Subject.objects.filter(prof=Profil.objects.get(user=request.user)).all()
+        case _:
+            subjects = Subject.objects.all()
+    return render(request, 'notes/subjects_note.html', {'subjects': subjects})
 
-def create_notes_view(request):
-    form= NoteCreateForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form= NoteCreateForm()
-
-
-    context={
-        'form':form
-    }
-    return render(request, 'create_notes.html', context)
+@login_required
+def edit_detail(request):
+    pass
 
