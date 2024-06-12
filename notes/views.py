71e2d35a7a4ab1c1caea980ed391c0b67a474cc3
TheGeekMax <maximesanciaume@gmail.com>
2024-06-12 10:13:20 +0200

    added fully working notes

--- notes/views.py
+++ notes/views.py
@@ -98,8 +98,28 @@ def get_notes(request,subject_id):
     notes = []
     # get subject group
     subject = Subject.objects.get(id=subject_id)
-    # get all students in the group
-    students = Profil.objects.filte
+    # get all students in the groups (subject.student_group is many to many field so we need to get all students in the group)
+    groups = subject.student_group.all()
+    students = []
+    for group in groups:
+        # add only if the student is not already in the list
+        for student in group.profil_set.all():
+            if student not in students and student.type == '0':
+                students.append(student)
+        
+    for student in students:
+        # get note if exists
+        note = -1
+        try:
+            note = int("{:,.0f}".format(Note.objects.get(profil=student,subject=subject).valeur))
+        except:
+            pass
+
+        notes.append({
+            "student":student,
+            "note":note
+        })
+    return notes
 
 
 # views
@@ -133,6 +153,35 @@ def edit_view(request):
     return render(request, 'notes/subjects_note.html', {'subjects': subjects})
 
 @login_required
-def edit_detail(request):
-    pass
+def edit_detail(request,id):
+    # test which role the user has
+    if Profil.objects.get(user=request.user).type == '0':
+        return redirect('notes:list.view')
+    elif Profil.objects.get(user=request.user).type == '1' and not Subject.objects.get(id=id).prof == Profil.objects.get(user=request.user):
+        return redirect('notes:edit.view')
+    # get all students notes
+    notes = get_notes(request,id)
+    
+    if request.method == 'POST':
+        for note in notes:
+            value = request.POST.get("note"+str(note["student"].id))
+            print(value)
+            if value == None or value == '':
+                continue
+            try:
+                note_obj = Note.objects.get(profil=note["student"],subject=Subject.objects.get(id=id))
+                note_obj.valeur = value
+                note_obj.save()
+            except:
+                note_obj = Note(
+                    profil=note["student"],
+                    subject=Subject.objects.get(id=id),
+                    valeur=value
+                )
+                note_obj.save()
+        return redirect('notes:edit.view')
+
+    # Render the template with the list of notes
+    return render(request, 'notes/subject_detail.html', {'notes': notes,'subject':Subject.objects.get(id=id)})
+    
 
